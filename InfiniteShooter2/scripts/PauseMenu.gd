f8942090dcff42f84c2f92ad21e5d9ef9650e417
PastThePixels <pastthescreen@gmail.com>
2021-07-26 20:58:59 -0600

    Made the pause menu (but no options yet!)

--- InfiniteShooter2/scripts/PauseMenu.gd
+++ InfiniteShooter2/scripts/PauseMenu.gd
@@ -4,5 +4,22 @@ func _input( event ):
 	
 	if event.is_action_pressed( "pause" ):
 		
+		if visible == true:
+			
+			fade_out()
+			yield( $Tween, "tween_completed" )
+		
 		visible = !visible
 		get_tree().paused = visible
+		fade_in()
+			
+
+func fade_in():
+	
+	$Tween.interpolate_property( $Background, "color", Color( 0, 0, 0, 0 ), Color( 0, 0, 0, .8 ), .3, Tween.TRANS_BACK, Tween.EASE_OUT )
+	$Tween.start()
+
+func fade_out():
+	
+	$Tween.interpolate_property( $Background, "color", Color( 0, 0, 0, .8 ), Color( 0, 0, 0, 0 ), .3, Tween.TRANS_BACK, Tween.EASE_IN )
+	$Tween.start()
