c60eccaed0bd98be2d57522db2d54a01df8e611f
PastThePixels <pastthescreen@gmail.com>
2021-07-28 14:41:39 -0600

    Set difficulty based on enemy type + made the enemies not shoot when a player is far from them

--- InfiniteShooter2/scripts/Enemy.gd
+++ InfiniteShooter2/scripts/Enemy.gd
@@ -7,6 +7,7 @@ var health = max_health
 
 # Engine vars
 onready var main = get_tree().get_root().get_node( "Main" )
+onready var player = get_tree().get_root().get_node( "Main/Game/Player" )
 var utils
 var enemy
 export (PackedScene) var Enemy1
@@ -27,14 +28,26 @@ func _ready():
 		
 		0:
 			enemy = Enemy1.instance()
+			
+			# Sets enemy stats
+			max_health = 1
+			health = max_health
 			$MovingTimer.wait_time = 0.002
 			
 		1:
 			enemy = Enemy2.instance()
+			
+			# Sets enemy stats
+			max_health = .5
+			health = max_health
 			$MovingTimer.wait_time = 0.003
 			
 		2:
 			enemy = Enemy3.instance()
+			
+			# Sets enemy stats
+			max_health = 2
+			health = max_health
 			$MovingTimer.wait_time = 0.001
 			
 	add_child( enemy )
@@ -62,7 +75,7 @@ func _process( delta ):
 
 func move_down():
 	
-	transform.origin.z += .1
+	transform.origin.z += .05
 	if transform.origin.z > utils.screen_to_local( Vector2( 0, utils.screen_size.y ) ).z:
 		
 		if get_parent().get_node( "Player" ) != null: get_parent().get_node( "Player" ).health -= health # This check makes sure if the player is still in the scene, meaning that is still alive.
@@ -86,9 +99,19 @@ func cleanup_ship():
 
 func fire_laser():
 	
+	# If the player is not close to the enemy (5 metres in this case), why shoot a laser? This helps with performance and also with basic logic
+	if main.has_node( "Game/Player" ) and abs( player.transform.origin.x - transform.origin.x ) > 5: return
+	
+	# Creating the laser
 	var laser = Laser.instance()
+	
+	# Telling the laser that it is not from the player + sets its damage
 	laser.from_player = false
+	laser.damage = damage
+	
+	# Setting the laser's position
 	laser.transform.origin = transform.origin
 	laser.transform.origin.z += 1 # Makes the laser come from the "top" of the ship instead of the center for added realism
-	laser.damage = damage
-	main.get_node( "Game" ).add_child( laser ) # Gets the scene "Main" and adds to it this laser
+	
+	# Gets the scene "Main" and adds to it this laser
+	main.get_node( "Game" ).add_child( laser )
