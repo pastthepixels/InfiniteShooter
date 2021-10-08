4dc89fa095dee96d2d6170252641d500be7bfce9
pastthepixels <pastthescreen@gmail.com>
2021-10-07 19:02:56 -0600

    Improved start screen/main menu

--- InfiniteShooter/scripts/MainMenu.gd
+++ InfiniteShooter/scripts/MainMenu.gd
@@ -1,8 +1,10 @@
-extends Control
+extends Node
 
 
 func _ready():
 	GameMusic.play_main()
+	get_node("../SkyScrolling").stop(true)
+	$AnimationPlayer.play("introduce-title")
 
 
 func _input(event):
@@ -10,77 +12,62 @@ func _input(event):
 	if (
 		(event is InputEventKey or event is InputEventJoypadButton)
 		and event.pressed
-		and has_node("StartScreen")
-		and has_node("StartScreen/LogoContainer")
+		and $Menu/StartScreen.visible == true
 	):
-		# Ensures the scrolling background is not playing
-		get_node("../SkyScrolling").stop(true)
-
 		# Plays the "gui-accept" sound
-		$SelectSquare/AcceptSound.play()
+		$Menu/SelectSquare/AcceptSound.play()
 
-		# Reparents the logo
-		var logo = $StartScreen/LogoContainer  # <-- This houses the logo, allowing it to be centered AND have an origin point in its middle.
-		$StartScreen.remove_child(logo)  # First we remove the logo from this object.
-		self.add_child(logo)  # Then we put it in the parent node.
-		logo.set_owner(self)  # Then we tell Godot that the logo's parent is `MainMenu`.
-		move_child(logo, 3)  # <-- Then we move the logo behind the leaderboard/upgrades screens
+		# Hides the start screen and shows the options/other stuff
+		$Menu/StartScreen.hide()
+		$Menu/Options.show()
+		
 
-		# Gets rid of this background
-		$StartScreen.queue_free()
-
-		# Slides the scale of the logo
-		logo.get_node("LogoSlide").play("LogoSlide")
-
-		# Slides the logo container -- DYNAMICALLY
-		var tween = Tween.new()  # Makes a tween node
-		add_child(tween)  # and adds it to the MainMenu node
-		# |- Does some fancy math to center the logo just 100 pixels above the middle
-		# v
-		tween.interpolate_property(
-			logo,
-			"rect_position",
-			logo.get_position(),
-			Vector2(logo.get_position().x, logo.get_position().y - 100),
-			1,
+		# Animations
+		$AnimationPlayer.play("switch")
+		$Tween.interpolate_property(
+			$Title,
+			"translation",
+			$Title.translation,
+			Vector3(0, 6, -2),
+			$AnimationPlayer.current_animation_length,
 			Tween.TRANS_QUAD,
 			Tween.EASE_OUT
 		)
-		tween.start()  # Starts it!
-
+		$Tween.start()
+		
 		# Allows things to be selected
-		$SelectSquare.show()
+		$Menu/SelectSquare.show()
 
 		# Returns the rest of this function
 		return
 
 	if event.is_action_pressed("ui_accept"):
-		$SelectSquare/AcceptSound.play()
-		match $Options.get_child($SelectSquare.index).name:  # Now we see which option has been selected...
+		$Menu/SelectSquare/AcceptSound.play()
+		match $Menu/Options.get_child($Menu/SelectSquare.index).name:  # Now we see which option has been selected...
 			"Play":  # If it is the one named "play", play the game.
 				SceneTransition.play(self, "play_game")
 
 			"Leaderboard":  # Same with selecting the leaderboard
-				if $Leaderboard.visible == false:
-					$Leaderboard.show_animated()
-					$SelectSquare.hide()
+				if $Menu/Leaderboard.visible == false:
+					$Menu/Leaderboard.show_animated()
+					$Menu/SelectSquare.hide()
 				else:
-					$Leaderboard.hide_animated()
-					$SelectSquare.show()
+					$Menu/Leaderboard.hide_animated()
+					$Menu/SelectSquare.show()
 
 			"Upgrades":  # Same with selecting the upgrades screen
-				if $Upgrades.visible == false:
-					$Upgrades.show_animated()
-					$SelectSquare.hide()
+				if $Menu/Upgrades.visible == false:
+					$Menu/Upgrades.show_animated()
+					$Menu/SelectSquare.hide()
 				else:
-					$Upgrades.handle_selection()
+					$Menu/Upgrades.handle_selection()
 
 			"Options":  # /options screen
-				if $OptionsMenu.visible == false:
-					$OptionsMenu.show_animated()
-					$SelectSquare.hide()
+				if $Menu/OptionsMenu.visible == false:
+					$Menu/OptionsMenu.show_animated()
+					$Menu/SelectSquare.hide()
 				else:
-					$OptionsMenu.handle_selection()
+					$Menu/OptionsMenu.handle_selection()
 
 			"Quit":  # Otherwise, quit the game
 				SceneTransition.play(self, "quit_game")
