8f20b1868785d186e7642a0b4616ae0a5fac36e8
pastthepixels <pastthescreen@gmail.com>
2021-10-08 22:16:13 -0600

    Made the countdown a bit better

--- InfiniteShooter/scripts/Countdown.gd
+++ InfiniteShooter/scripts/Countdown.gd
@@ -23,7 +23,7 @@ func _ready():
 			for _flash in range(0, 3): # number of flashes
 				$AnimationPlayer.stop(true)
 				$AnimationPlayer.play("show") # Now we fade in the text
-				yield(get_tree().create_timer(.4), "timeout")  # and then wait a bit for you to be able to read the text
+				yield(get_tree().create_timer(.3), "timeout")  # and then wait a bit for you to be able to read the text
 	get_node("../PauseMenu").disabled = false
 	emit_signal("finished") # Once we are done the loop, we signal that we are done the countdown
 	queue_free() # and then remove the countdown node
