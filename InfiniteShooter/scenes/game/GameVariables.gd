3509f6106bd35830a0132bf417ecca968a666dfb
pastthepixels <pastthescreen@gmail.com>
2021-11-22 23:58:08 -0700

    Modified main menu soundtrack and also fixed up GameVariables.gd back to normal

--- InfiniteShooter/scenes/game/GameVariables.gd
+++ InfiniteShooter/scenes/game/GameVariables.gd
@@ -4,9 +4,9 @@ extends Node
 
 var enemy_difficulty = 1 # default 1
 
-var waves_per_level = 1  # default 5
+var waves_per_level = 5  # default 5
 
-var enemies_per_wave = 2 # default 20
+var enemies_per_wave = 20 # default 20
 
 var enemies_on_screen_range = [2, 10] # [min=2,max=10]
 
