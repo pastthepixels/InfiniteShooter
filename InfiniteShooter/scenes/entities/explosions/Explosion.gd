e90f764c1acbb9bf831eeb68241f5c27327857f2
pastthepixels <pastthescreen@gmail.com>
2022-01-27 14:25:48 -0700

    Fixed a bug where mini-laser-explosions would follow lasers

--- InfiniteShooter/scenes/entities/explosions/Explosion.gd
+++ InfiniteShooter/scenes/entities/explosions/Explosion.gd
@@ -2,9 +2,11 @@ extends Spatial
 
 export var auto_explode = false
 
+export var auto_delete = false
+
 export var static_position = false
 
-export var only_visuals = false
+export var no_sound = false
 
 export var speed_scale = 1
 
@@ -19,11 +21,15 @@ func _ready():
 	$ExplosionAnimation.frames.set_animation_speed("default", 50*speed_scale) # Default is 50 FPS
 	if auto_explode == true:
 		explode()
-
-func _process(_delta):
 	if static_position == true:
-		translation = original_transform.origin - get_parent().translation
-		
+		# 1. Create a new node with the current node's position
+		var new_node = duplicate()
+		new_node.static_position = false
+		new_node.translation = global_transform.origin
+		# 2. Add that node to the GameSpace node and then remove this node as it is obsolete
+		var new_parent = get_node("/root/Game/GameSpace")
+		new_parent.add_child(new_node)
+		queue_free()
 
 func explode():
 	if $ExplosionAnimation.playing == false and $ExplosionSound.playing == false:
@@ -32,16 +38,17 @@ func explode():
 		$ExplosionAnimation.playing = true
 
 		# Vibrates the first controller and shakes the screen
-		if only_visuals == false:
+		if no_sound == false:
 			Input.start_joy_vibration(0, 0.5, 0.7, .2)
 			CameraEquipment.get_node("ShakeCamera").add_trauma(.4)
 		
 		# Plays an explosion sound at a random pitch
 		$ExplosionSound.pitch_scale = rand_range(0.8, 1.2)
-		if only_visuals == false: $ExplosionSound.play()
-		
-		# Waits for animations/sounds to finish
-		if $ExplosionAnimation.playing == true: yield($ExplosionAnimation, "animation_finished")
-		hide()
-		if $ExplosionSound.playing == true: yield($ExplosionSound, "finished")
-		emit_signal("exploded")
+		if no_sound == false: $ExplosionSound.play()
+
+func _on_ExplosionAnimation_animation_finished():
+	# Waits for animations/sounds to finish
+	hide()
+	if $ExplosionSound.playing == true: yield($ExplosionSound, "finished")
+	emit_signal("exploded")
+	if auto_delete == true: queue_free()
