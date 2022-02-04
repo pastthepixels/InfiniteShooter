d7c0524ada4170cbe164b7a32123f33d42828a0c
pastthepixels <pastthescreen@gmail.com>
2022-02-03 21:38:03 -0700

    Fixed some animations affecting Utils.recalculate + too much warping in fullscreen mode

--- InfiniteShooter/CameraEquipment.gd
+++ InfiniteShooter/CameraEquipment.gd
@@ -1,4 +1,12 @@
 extends Spatial
 
+onready var initial_warp_amount = $LensDistortion.material.get_shader_param("warp_amount")
+
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN) # Hides the cursor
+
+func _process(_delta):
+	if OS.window_fullscreen == true:
+		$LensDistortion.material.set_shader_param("warp_amount", -0.001)
+	elif $LensDistortion.material.get_shader_param("warp_amount") == 0:
+		$LensDistortion.material.set_shader_param("warp_amount", initial_warp_amount)
