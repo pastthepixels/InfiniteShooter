47f5bfdb2bb2775e6fb49268e170fc945d8cc660
pastthepixels <pastthescreen@gmail.com>
2022-02-11 23:25:28 -0700

    Update CameraEquipment.gd

--- InfiniteShooter/CameraEquipment.gd
+++ InfiniteShooter/CameraEquipment.gd
@@ -4,9 +4,3 @@ onready var initial_warp_amount = $LensDistortion.material.get_shader_param("dis
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN) # Hides the cursor
-
-func _process(_delta):
-	if OS.window_fullscreen == true:
-		pass#$LensDistortion.material.set_shader_param("distort", -0.001)
-	elif $LensDistortion.material.get_shader_param("distort") == 0:
-		pass#$LensDistortion.material.set_shader_param("distort", initial_warp_amount)
