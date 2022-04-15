4fd75140311971deebdb1c72d1e3bd02ec7546fa
PastThePixels <pastthescreen@gmail.com>
2022-04-15 11:42:41 -0600

    Made a (heavily WIP) loading screen.
    This *might* work? It works like the ParticlesCache node, forcing Godot to instance all scenes first. (My logic is that since there are lag spikes when instancing scenes for the first time we can just instance EVERY scene for the first time under a loading screen so users know to expect lag)

---
+++ InfiniteShooter/scenes/loading/LoadingScreen.gd
@@ -0,0 +1,44 @@
+extends Spatial
+
+export var file_path = "res://scenes/entities/" # Has to have that forward slash
+
+var scenes = []
+
+func _ready():
+	get_tree().paused = true
+
+func _on_InitTimer_timeout():
+	loop_scenes(file_path)
+	instance_scenes_then_quit()
+
+func loop_scenes(path): # from https://docs.godotengine.org/en/3.1/classes/class_directory.html
+	var dir = Directory.new()
+	if dir.open(path) == OK:
+		dir.list_dir_begin()
+		var file_name = dir.get_next()
+		while (file_name != ""):
+			if dir.current_is_dir() and ("." in file_name) == false:
+				loop_scenes(path + file_name)
+			elif ".tscn" in file_name:
+				scenes.append(load(path + "/" + file_name))
+				print("Loaded scene " + path + "/" + file_name)
+			file_name = dir.get_next()
+	else:
+		print("An error occurred when trying to access the path.")
+
+func instance_scenes_then_quit():
+	for scene_path in scenes:
+		var scene = scene_path.instance()
+		if scene.is_in_group("enemies"):
+			scene.initialize(1)
+		if scene.is_in_group("explosions"):
+			scene.explode()
+		scene.set_pause_mode(PAUSE_MODE_STOP)
+		add_child(scene)
+		$Timer.start()
+		yield($Timer, "timeout")
+		scene.queue_free()
+	$Timer.start()
+	yield($Timer, "timeout")
+	get_tree().paused = false
+	queue_free()
