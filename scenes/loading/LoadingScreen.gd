390c4cca8ab94d8f3f32fe4fdfee599efca93a2f
PastThePixels <pastthescreen@gmail.com>
2022-08-09 11:30:38 -0600

    Edited LoadingScreen to not produce as much errors

--- scenes/loading/LoadingScreen.gd
+++ scenes/loading/LoadingScreen.gd
@@ -8,6 +8,7 @@ func _ready():
 	get_tree().paused = true
 	CameraEquipment.get_node("ShakeCamera").ignore_shake = true
 	AudioServer.set_bus_mute(AudioServer.get_bus_index("Master"), true)
+	$InitTimer.start()
 
 func _on_InitTimer_timeout():
 	loop_scenes(file_path)
@@ -31,21 +32,21 @@ func loop_scenes(path): # from https://docs.godotengine.org/en/3.1/classes/class
 func instance_scenes_then_quit():
 	var progress = 0
 	for scene_path in scenes:
-		yield(get_tree(),"idle_frame")
+		yield(get_tree(), "idle_frame")
 		$Cover/Label/ProgressBar.max_value = len(scenes) - 1
 		$Cover/Label/ProgressBar.value = progress
 		progress += 1
 		#
 		var scene = scene_path.instance()
+		$Instances.add_child(scene)
 		if scene.is_in_group("enemies") or scene.is_in_group("bosses"):
 			scene.initialize(1)
 			if scene.is_in_group("enemies"): scene._on_LaserTimer_timeout()
-			scene.explode_ship()
+			#scene.explode_ship()
 		if scene.is_in_group("players"):
 			scene.fire_laser()
 		if scene.is_in_group("explosions"):
 			scene.explode()
-		$Instances.add_child(scene)
 	$ExitTimer.start()
 
 func _on_ExitTimer_timeout():
