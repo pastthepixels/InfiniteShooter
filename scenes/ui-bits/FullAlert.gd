116b95048480acf5429918d7f4f9f64a36355383
pastthepixels <pastthescreen@gmail.com>
2022-09-02 18:26:34 -0600

    Port FullAlert.tscn to the new UI

--- scenes/ui-bits/FullAlert.gd
+++ scenes/ui-bits/FullAlert.gd
@@ -2,10 +2,6 @@ extends Control
 
 var pause_previous_state = false
 
-export var enable_exiting = false
-
-onready var initial_background_color = $Background.color
-
 var signal_to_emit
 
 # warning-ignore:unused_signal
@@ -13,53 +9,6 @@ signal exited
 
 # warning-ignore:unused_signal
 signal confirmed
-
-func _process(_delta):
-	if visible == true:
-		$Foreground/Highlight.visible = Input.is_action_pressed("ui_accept") or Input.is_action_pressed("ui_cancel")
-
-func _input(event):
-	if visible == true and (event.is_action_pressed("ui_cancel") or event.is_action_pressed("ui_accept")):
-		$KeyDownSound.play()
-	
-	if visible == true and (event is InputEventKey or event is InputEventJoypadButton):
-		if enable_exiting and event.is_action_released("ui_cancel"):
-			signal_to_emit = "exited"
-			$Background.color = Color("#32ff0000")
-			$AnimationPlayer.play_backwards("fade")
-			$AcceptSound.play()
-		elif event.is_action_released("ui_accept"):
-			signal_to_emit = "confirmed"
-			if enable_exiting == true: $Background.color = Color("#3200ff00")
-			$AnimationPlayer.play_backwards("fade")
-			$AcceptSound.play()
-
-func alert(text, can_be_exited=false):
-	pause_previous_state = get_tree().paused
-	get_tree().paused = true
-	enable_exiting = can_be_exited
-	if can_be_exited == true:
-		$Foreground/PanelContainer/Content/Escape.text = "Press the escape key to cancel this operation"
-	else:
-		$Foreground/PanelContainer/Content/Escape.text = "Press the confirm key to continue"
-	$Background.color = initial_background_color
-	$Foreground/PanelContainer/Content/MarginContainer/Alert.text = text
-	$AnimationPlayer.play("RESET")
-	$AnimationPlayer.play("fade")
-	$Sound.play()
-	update_corners()
-
-func update_corners():
-	# Waits to make sure $Foreground/PanelContainer is automatically resized
-	yield(get_tree(),"idle_frame") # I copied and pasted this a bunch of times and it "just works", so I'll just leave this here.
-	yield(get_tree(),"idle_frame")
-	yield(get_tree(),"idle_frame")
-	# Sets the new size for the corners as a minimum size so that the responsive UI doesn't override it
-	$Foreground/Corners.rect_min_size = $Foreground/PanelContainer.rect_size
-	$Foreground/Highlight.rect_min_size = $Foreground/PanelContainer.rect_size
-	# Forces an update to resize the corners
-	$Foreground/Corners.rect_size = Vector2()
-	$Foreground/Highlight.rect_size = Vector2()
 
 
 func _on_AnimationPlayer_animation_started(anim_name):
@@ -75,3 +24,32 @@ func _on_AnimationPlayer_animation_finished(_anim_name):
 		get_tree().paused = pause_previous_state
 		hide()
 		emit_signal(signal_to_emit)
+
+
+func _on_Yes_pressed():
+	signal_to_emit = "confirmed"
+	$AnimationPlayer.play_backwards("fade")
+
+
+func _on_No_pressed():
+	signal_to_emit = "exited"
+	$AnimationPlayer.play_backwards("fade")
+
+
+func _on_Ok_pressed():
+	signal_to_emit = "exited"
+	$AnimationPlayer.play_backwards("fade")
+
+func alert(text, is_confirmation=false):
+	pause_previous_state = get_tree().paused
+	get_tree().paused = true
+	if is_confirmation == true:
+		get_node("%ConfirmationButtons").show()
+		get_node("%ConfirmationButtons/HBoxContainer/Yes").grab_focus()
+	else:
+		get_node("%DismissButtons").show()
+		get_node("%ConfirmationButtons/HBoxContainer/Ok").grab_focus()
+	$Foreground/PanelContainer/Content/MarginContainer/Alert.text = text
+	$AnimationPlayer.play("RESET")
+	$AnimationPlayer.play("fade")
+	$Sound.play()
