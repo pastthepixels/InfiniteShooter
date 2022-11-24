1e506a0a50f5d1af212b1551d77254de3a24325b
PastThePixels <pastthescreen@gmail.com>
2022-11-23 17:43:36 -0700

    Update the mouse test with a separate MouseControls node

---
+++ scenes/entities/player/MouseControls.gd
@@ -0,0 +1,47 @@
+extends Node
+
+signal mouse_moved(mouse_intensity)
+
+export(float) var sensitivity = 1.0
+
+export(int) var max_mouse_speed = 500 # Maximum movement in pixels/second
+
+export(bool) var pointer_lock_enabled = false
+
+export(int) var frames_until_mouse_reset = 1
+
+var timer = frames_until_mouse_reset
+
+var mouse_moved : bool = false
+
+var mouse_intensity : Vector2 setget ,get_mouse_intensity
+
+var _mouse_intensity : Vector2
+
+
+
+# _input: Calculate the intensity of each mouse movement and multiply it by -1 because for some reason down is positive for Godot???
+func _input(event):
+	# Pointer lock
+	if pointer_lock_enabled == true and event is InputEventMouseButton and event.pressed == true:
+		if event.button_index == 1:
+			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+		else:
+			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	
+	if event is InputEventMouseMotion:
+		mouse_moved = true
+		var mouse_speed = event.relative / get_process_delta_time() * -1
+		var radius = clamp(abs(mouse_speed.length() * sensitivity), 0, max_mouse_speed) / max_mouse_speed # Divides by max_mouse_movement (and clamps) to get a value out of 1 (that can't be higher than 1)
+		_mouse_intensity = Vector2(radius, 0).rotated(mouse_speed.angle())
+		emit_signal("mouse_moved", _mouse_intensity)
+
+func get_mouse_intensity():
+	return _mouse_intensity if mouse_moved == true else Vector2(0, 0)
+
+func _process(delta):
+	if mouse_moved == true and timer == 0:
+		mouse_moved = false
+		timer = frames_until_mouse_reset
+	elif mouse_moved == true:
+		timer -= 1
--- scenes/entities/player/MouseTest.gd
+++ scenes/entities/player/MouseTest.gd
@@ -1,53 +1,23 @@
-extends Node
+extends Spatial
 
-export var max_mouse_movement = 25 # Maximum movement in the X or Y direction
+export(float, 0.1, 1.0) var speed = 0.2 # m/s
 
-export var speed = 1
-
-export var interpolation_time = 4
-
-var mouse_moved = false
-
-var last_mouse_move_relative : Vector2
-var mouse_move_relative_interpolated : Vector2 # Interpolated to the last position the mouse was moved to
-var mouse_intensity : Vector2 # Mouse velocity, normalized (mouse_velocity_pixels / max_mouse_movement)
+export(float) var speed_cursor = 200 # m/s
 
 func _ready():
-	if has_node("/root/MouseTest"): LoadingScreen.disable()
-
-func _input(event):
-	if event is InputEventMouseButton and event.pressed == true:
-		if event.button_index == 1:
-			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
-	if event.is_action_pressed("pause"):
-		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
-	
-	if event is InputEventMouseMotion:
-		mouse_moved = true
-		last_mouse_move_relative = Vector2(
-			clamp(event.relative.y, -max_mouse_movement, max_mouse_movement), 
-			clamp(event.relative.x, -max_mouse_movement, max_mouse_movement)
-		) * -1
-
+	pass#LoadingScreen.disable()
 
 func _process(delta):
-	# Check if the mouse was just moved and reset last_mouse_move if it wasn't moved at all
-	if mouse_moved:
-		mouse_moved = false
-	else:
-		last_mouse_move_relative = Vector2(0, 0)
+#	$MeshInstance.rotation_degrees.x = -$MouseControls.mouse_intensity.y * 90
+#	$MeshInstance.rotation_degrees.z = $MouseControls.mouse_intensity.x * 90
 	
-	# Interpolate to the last position the mouse moved to 
-	mouse_move_relative_interpolated = mouse_move_relative_interpolated.linear_interpolate(last_mouse_move_relative, interpolation_time * delta)
-	mouse_intensity = mouse_move_relative_interpolated / max_mouse_movement
+	$MeshInstance.translation.x -= $MouseControls.get_mouse_intensity().x * speed
+	$MeshInstance.translation.z -= $MouseControls.get_mouse_intensity().y * speed
 	
-	# Printing and moving/rotating
-	$Control/Label.text = "(%.2f, %.2f)" % [mouse_intensity.x, mouse_intensity.y]
-	
-	$Spatial/MeshInstance.rotation_degrees.x = -mouse_intensity.x * 60
-	$Spatial/MeshInstance.rotation_degrees.z = mouse_intensity.y * 60
-	
-	$Spatial/MeshInstance.translation.x -= mouse_intensity.y / 10 * speed
-	$Spatial/MeshInstance.translation.z -= mouse_intensity.x / 10 * speed
-	
-	$Spatial/MeshInstance/Label3D.text = "(%.2f, %.2f)" % [$Spatial/MeshInstance.translation.x, $Spatial/MeshInstance.translation.z]
+	$MeshInstance/Label3D.text = "(%.2f, %.2f)" % [$MeshInstance.translation.x, $MeshInstance.translation.z]
+
+
+func _on_MouseControls_mouse_moved(mouse_intensity):
+	print(mouse_intensity)
+	$Cursor.position.x -= mouse_intensity.x * speed_cursor
+	$Cursor.position.y -= mouse_intensity.y * speed_cursor
