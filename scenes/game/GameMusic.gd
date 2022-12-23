bf84b9a9b203c8137b8e3353141b62107bd7cf96
PastThePixels <pastthescreen@gmail.com>
2022-12-22 21:41:45 -0700

    Improve randomization of game songs

--- scenes/game/GameMusic.gd
+++ scenes/game/GameMusic.gd
@@ -4,19 +4,39 @@ export(Array, AudioStream) var game_songs
 
 export(bool) var autoswitch = true
 
+var previous_game_songs = []
+
 func _ready(): # Ensuring that NO SONG loops the same
 	randomize()
 	for song in game_songs: song.set_loop(false)
 
+# Ensures that each song has at least len(game_songs) before it can be heard again
+func pick_game_song():
+	# Clears previous_game_songs if full (every len(game_songs))
+	if len(previous_game_songs) == len(game_songs):
+		previous_game_songs = []
+	
+	var game_song = game_songs[randi() % game_songs.size()]
+	
+	# Calls the function (recursively) if the song already has been played before
+	if game_song in previous_game_songs:
+		return pick_game_song()
+		
+	# Otherwise adds the song to previous_game_songs
+	previous_game_songs.append(game_song)
+	
+	return game_song
+
 
 func start_game():
-	crossfade(game_songs[randi() % game_songs.size()])
+	crossfade(pick_game_song())
+
 
 
 func switch_game():
 	stop()
 	var old_stream = stream
-	var new_stream = game_songs[randi() % game_songs.size()]
+	var new_stream = pick_game_song()
 	if old_stream == new_stream:# Preventing duplicate songs
 		switch_game()
 	else:
