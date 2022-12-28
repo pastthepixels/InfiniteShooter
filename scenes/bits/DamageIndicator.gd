5b635568766c14e038370dbe70a3917f41f581b8
PastThePixels <pastthescreen@gmail.com>
2022-12-27 22:15:58 -0700

    Create a damage indicator node

---
+++ scenes/bits/DamageIndicator.gd
@@ -0,0 +1,51 @@
+extends Label3D
+
+export(bool) var free_once_complete = false
+
+export(float) var animation_length = 0.500 # in milliseconds
+
+var global_position
+
+func _ready():
+	visible = false
+
+func activate(text=""):
+	global_position = global_transform.origin
+	set_as_toplevel(true)
+	self.text = text
+	show()
+	animate()
+
+func animate():
+	var rand_pos = Vector3(2*randf() - 1, 0, 2*randf() - 1)
+	$Tween.interpolate_property(
+		self,
+		"translation",
+		global_position,
+		global_position + rand_pos,
+		animation_length,
+		Tween.TRANS_LINEAR,
+		Tween.EASE_OUT
+	)
+	$Tween.interpolate_property(
+		self,
+		"modulate:a",
+		1,
+		0,
+		animation_length,
+		Tween.TRANS_LINEAR,
+		Tween.EASE_OUT
+	)
+	$Tween.interpolate_property(
+		self,
+		"outline_modulate:a",
+		1,
+		0,
+		animation_length,
+		Tween.TRANS_LINEAR,
+		Tween.EASE_OUT
+	)
+	$Tween.start()
+
+func _on_Tween_tween_all_completed():
+	if free_once_complete == true: queue_free()
